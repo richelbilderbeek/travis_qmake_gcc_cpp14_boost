dfee1e1ec306ca7f7934bb9bec01b14ccdb7f557
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-19 14:29:42 +0100

    Made code C++14

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <boost/graph/adjacency_list.hpp>
 
-int main() {
+auto f() noexcept
+{
   boost::adjacency_list<> g;
   boost::add_vertex(g);
-  if (boost::num_vertices(g) != 1) return 1;
+  return boost::num_vertices(g);
+}
+
+int main() {
+  if (f() != 1) return 1;
 }
